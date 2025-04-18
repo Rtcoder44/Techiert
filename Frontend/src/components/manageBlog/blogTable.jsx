b5e3ce45a8e6b3c64aba0c6c06914ab0d505476e
Calculater-App <ritik.guptabca21@smslucknow.ac.in>
2025-04-19 00:06:46 +0530

    minor changes

--- Frontend/src/components/manageBlog/blogTable.jsx
+++ Frontend/src/components/manageBlog/blogTable.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "../../context/authContext";
 import BlogEditModal from "../manageBlog/blogEditModal";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
