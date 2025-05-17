bb5cac462da21d4bc778d5b79f5be66d730b9033
Calculater-App <ritik.guptabca21@smslucknow.ac.in>
2025-05-17 16:14:16 +0530

    added some improvement in the signle post page

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from '@tailwindcss/typography';
+
 export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -6,7 +8,19 @@ export default {
       fontFamily: {
         playfair: ['"Playfair Display"', 'serif'],
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            img: {
+              margin: 'auto',
+              borderRadius: theme('borderRadius.lg'),
+              maxWidth: '100%',
+              height: 'auto',
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [],
+  plugins: [typography],
 };
